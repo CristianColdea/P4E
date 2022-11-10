40bc60572e67e785adf2009635403c24d8126245
CristianColdea <48172648+CristianColdea@users.noreply.github.com>
2022-11-10 22:21:21 +0200

    Add files via upload

---
+++ chapter11/re_three.py
@@ -0,0 +1,16 @@
+# Search for lines that have an at sign between characters
+
+import re
+hand = open('mbox-short.txt')
+"""
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('[a-zA-Z0-9]\S+@\S+[a-zA-Z]', line)
+    if len(x) > 0:
+        print(x)
+"""
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('^X\S*: ([0-9.]+)', line)
+    if len(x) > 0:
+        print(x)
